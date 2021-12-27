9d4c353e91acb80d5d79d29b590f3df909fea41a
Nguyen Cong Minh <nguyencongminh050@gmail.com>
2021-12-27 19:47:09 +0700

    27/12/2021

--- pygomo.py
+++ pygomo.py
@@ -1,3 +1,90 @@
-class Engine(object):
-    def __init__(self):
+import subprocess
+
+
+class Engine:
+    def __init__(self, engine_name):
+        self.engine_name = engine_name
+        self.engine = subprocess.Popen(engine_name, stdin=subprocess.PIPE,
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       bufsize=1, universal_newlines=True)
+        self.protocol = Protocol(self.engine)
+
+    # Kill process of engine
+    def kill_engine(self):
+        import psutil
+
+        def findProcessIdByName(_processName):
+            """
+            Get a list of all the PIDs of a all the running process whose name contains
+            the given string _processName
+            """
+            listOfProcessObjects = []
+            # Iterate over the all the running process
+            for process in psutil.process_iter():
+                try:
+                    pinfo = process.as_dict(attrs=['pid', 'name', 'create_time'])
+                    # Check if process name contains the given name string.
+                    if _processName.lower() in pinfo['name'].lower():
+                        listOfProcessObjects.append(pinfo['pid'])
+                except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                    pass
+            return listOfProcessObjects
+
+        try:
+            for i in findProcessIdByName(self.engine_name):
+                _proc = psutil.Process(i)
+                _proc.terminate()
+        except:
+            pass
+
+
+class Protocol:
+    def __init__(self, engine):
+        self.engine = engine
+        self.info_dict = {
+            'timeout_match': 0,
+            'timeout_turn': 0,
+            'game_type': 0,
+            'rule 1': 0,
+            'time_left': 0,
+            'max_memory': 0,
+        }
+
+    # Send input to engine
+    def send(self, command: str):
+        self.engine.stdin.write(command + '\n')
+
+    # Receive output
+    def receive(self, stdout=True, stderr=False):
+        if stdout:
+            return self.engine.stdout.readline()
+        elif stderr:
+            return self.engine.stderr.readline()
+
+    def about(self):
+        # Get info about engine
+        self.send('ABOUT')
+        info = self.receive()
+        return info
+
+    def set_info(self):
+
         pass
+
+    def exit(self):
+        # Exit engine
+        self.send('END')
+
+
+# Example
+def main():
+    engine = Engine('embryo.exe')
+    about = engine.protocol.about()
+    engine.protocol.exit()
+    print(about)
+    return
+
+
+if __name__ == '__main__':
+    main()
